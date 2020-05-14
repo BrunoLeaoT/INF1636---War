918a9ed178086c8c3a76be554057c1be2e611e1b
Stefano <Stefano@DESKTOP-5L2O3HO>
2020-05-14 20:23:17 -0300

    CartaObject e Cartas agora sao uma classe unica.

--- src/Model/CartaObject.java
+++ src/Model/CartaObject.java
@@ -1,18 +1,52 @@
 package Model;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 enum Forma{
 	QUADRADO, CIRCULO, TRIANGULO;
 }
+
 class CartaObject {
+	
 	private String nomeTerritorio;
 	private Forma forma;
 	
+	// Deck de cartas.
+	static private ArrayList<CartaObject> Deck; 
+	
 	public CartaObject(String nome,Forma forma) {
-		// TODO Auto-generated constructor stub
 		nomeTerritorio = nome;
 		this.forma = forma;
+		
+		// Inicializa deck.
+		Deck = new ArrayList<CartaObject>();
+		Deck.add(new CartaObject("África do Sul", Forma.TRIANGULO));
+		Deck.add(new CartaObject("Alaska", Forma.TRIANGULO));
+		Deck.add(new CartaObject("Alemanha", Forma.CIRCULO));
+		Deck.add(new CartaObject("Aral", Forma.TRIANGULO));
+		Deck.add(new CartaObject("Argélia", Forma.CIRCULO));
+		Deck.add(new CartaObject("Argentina", Forma.QUADRADO));
+		Deck.add(new CartaObject("Austrália", Forma.TRIANGULO));
+		Deck.add(new CartaObject("Bolívia", Forma.TRIANGULO));
+	}
+	
+	static public void ShuffleDeck()
+	{
+		Collections.shuffle(Deck);
+	}
+	
+	public void distribuirCartas(ArrayList<Jogador> jogadores) {
+		if(jogadores.size() <= 0)
+			throw new IllegalArgumentException("Quantidade de jogadores deve ser maior que zero");
+		for (int i = 0; i < Deck.size(); i++) {
+			for(int j = 0; j < jogadores.size();j++) {
+				if(Deck.size() == i)
+					break;
+				jogadores.get(j).getCartas().add(Deck.get(i));
+				i++;
+			}
+		}
 	}
 	
 	public boolean podeTrocarCarta(ArrayList<CartaObject> cartas) {
