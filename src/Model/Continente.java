7b0d9d93075f88a661df301bbf7854f02ac7e350
Stefano <Stefano@DESKTOP-5L2O3HO>
2020-05-30 19:16:06 -0300

    added Continente names

--- src/Model/Continente.java
+++ src/Model/Continente.java
@@ -2,18 +2,20 @@ package Model;
 
 public enum Continente
 {
-	Africa(2, 19),
-	AmericaDoNorte(4, 8),
-	AmericaDoSul(3, 6),
-	Asia(5, 15),
-	Europa(3, 8),
-	Oceania(2, 4);
+	Africa("África", 2, 19),
+	AmericaDoNorte("América do Norte", 4, 8),
+	AmericaDoSul("América do Sul", 3, 6),
+	Asia("Asia", 5, 15),
+	Europa("Europa", 3, 8),
+	Oceania("Oceania", 2, 4);
 	
+	public final String Nome;
 	public final int Bonus;
 	public final int NumeroTerritorios;
 	
-	private Continente(int bonustropa, int qtdTerritorios)
+	private Continente(String nome, int bonustropa, int qtdTerritorios)
 	{
+		Nome = nome;
 		Bonus = bonustropa;
 		NumeroTerritorios = qtdTerritorios;
 	}
