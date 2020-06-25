611efd9ffecf08557e64940627cfe2a27c5e210c
Stefano <Stefano@DESKTOP-5L2O3HO>
2020-06-24 21:59:47 -0300

    Refatoracao Jogadores

--- src/Model/Jogador.java
+++ src/Model/Jogador.java
@@ -5,54 +5,80 @@ import java.util.Collections;
 import java.util.Scanner;
 
 class Jogador {
-	static ArrayList<Jogador> jogadores;
-
-	String nome;
+		
+	private String nome;
 	private Cor cor;
-	Objetivo objetivo;
+	private Objetivo objetivo;
 	private ArrayList<Carta> cartas;
 	private ArrayList<Territorio> Territorios;
 	
-	public Jogador() {
-		jogadores = new ArrayList<Jogador>();
-	}
-	public Jogador(String nomeRecebido, Cor corRecebida, Objetivo objetivo2) {
+	public Jogador(String nomeRecebido, Cor corRecebida) {
+		
 		if(nomeRecebido.equals(""))
 			throw new IllegalArgumentException("Nome não pode ser vazio");
-		if(jaSelecionaramCor(corRecebida))
-			throw new IllegalArgumentException("Cor já foi selecionada");
+		
 		nome = nomeRecebido;
 		cor = corRecebida;
-		objetivo = objetivo2;
 		cartas = new ArrayList<Carta>();
-		jogadores.add(this);
+	}
+
+	public Objetivo getObjetivo() {
+		return objetivo;
 	}
 	
-	//Antes de começar randomizar o os jogadores para determinar ordem
-	public void randomizarJogadores() {
-		Collections.shuffle(jogadores);
+	public void setObjetivo(Objetivo o)
+	{
+		objetivo = o;
 	}
 	
-	private boolean jaSelecionaramCor(Cor cor) {
-		for(int i=0;i<jogadores.size();i++) {
-			if(jogadores.get(i).cor.compareTo(cor) == 0)
-				return true;
-		}
-		return false;
-	}
-	
-	public ArrayList<Jogador> getJogadores() {
-		return jogadores;
-	}
-	public ArrayList<Carta> getCartas() {
-		return cartas;
+	public String getNome() {
+		return nome;
 	}
 	
 	public Cor getCor() {
 		return cor;
 	}
 	
-	public boolean HasContinente(Continente cont)
+	public ArrayList<Carta> getCartas() {
+		return cartas;
+	}
+	
+	public void addTerritorio(Territorio novo) {
+		Territorios.add(novo);
+	}
+	
+	// Territorios devem ter o mesmo hash; so pode haver uma instancia de terr no codigo.
+	public void rmTerritorio(Territorio velho) {
+		Territorios.remove(velho);
+	}
+	
+	public int calculaTropasDisponiveis()
+	{
+		int tropas = 0;
+		tropas += calculaTropasTerritorios();
+		tropas += calculaTropasContinentes();
+		return tropas;
+	}
+	
+	private int calculaTropasTerritorios()
+	{
+		return Territorios.size()/2;
+	}
+	
+	private int calculaTropasContinentes()
+	{
+		int bonusContinente = 0;
+		
+		for (Continente c : Continente.values())
+		{
+			if(hasContinente(c))
+				bonusContinente += c.Bonus;
+		}
+		
+		return bonusContinente;
+	}
+	
+	public boolean hasContinente(Continente cont)
 	{
 		int count = 0;
 		for(Territorio t : Territorios)
@@ -66,12 +92,9 @@ class Jogador {
 		else
 			return false;
 	}
-	public Jogador getJogadorPorNome(String nomeJogador) {
-		for (int i = 0; i < jogadores.size(); i++) {
-			if(nomeJogador.compareTo(jogadores.get(i).nome) == 0) {
-				return jogadores.get(i);
-			}
-		}
-		return null;
+	
+	public void fazerTrocaCartas() throws Exception
+	{
+		throw new Exception("not implemented");
 	}
 }
---
+++ src/Model/Jogadores.java
@@ -0,0 +1,60 @@
+package Model;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+// Singleton simples (nao thread safe)
+public class Jogadores 
+{
+	static private Jogadores singleton;
+	private ArrayList<Jogador> ListaJogadores;
+	private int qtdJogadores;
+	
+	static public Jogadores getInstancia()
+	{
+		if (singleton == null)
+			singleton = new Jogadores();
+		return singleton;
+	}
+	
+	private Jogadores()
+	{
+		ListaJogadores = new ArrayList<Jogador>();
+		qtdJogadores = 0;
+	}
+	
+	public void addJogador(Jogador j) throws Exception
+	{
+		if(qtdJogadores > 6)
+			throw new Exception("O jogo não pode ter mais de 6 jogadores");
+		
+		ListaJogadores.add(j);
+		qtdJogadores++;
+	}
+	
+	public Jogador selectJogadorByIndex(int index)
+	{
+		return ListaJogadores.get(index);
+	}
+	
+	public int getQtdJogadores()
+	{
+		return qtdJogadores;
+	}
+	
+	// Retorna lista de cores (de jogadores) existentes na partida 
+	// Obs: lista é ordenada de acordo com a lista de jogadores 
+	public ArrayList<Cor> getJogadoresCor()
+	{
+		ArrayList<Cor> cores = new ArrayList<Cor>();
+		for(Jogador j : ListaJogadores)
+			cores.add(j.getCor());
+		return cores;
+	}
+	
+	public void shuffleJogadores()
+	{
+		Collections.shuffle(ListaJogadores);
+	}
+	
+}
