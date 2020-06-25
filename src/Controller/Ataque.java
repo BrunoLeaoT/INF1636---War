a09f32edcd2cb80d1adbe7fdecc064465e3afd0a
Stefano <Stefano@DESKTOP-5L2O3HO>
2020-06-24 22:05:25 -0300

    Inicio refatoracao Ataque (em desenvolvimento)

--- src/Controller/Ataque.java
+++ src/Controller/Ataque.java
@@ -1,9 +1,11 @@
 package Controller;
 
 import Model.Geral;
+import Model.Partida;
 
 public class Ataque {
-	public Geral geral;
+	private Geral geral;
+	private Partida partida;
 	public Ataque() {
 		geral = Geral.getGeral();
 	}
@@ -16,5 +18,27 @@ public class Ataque {
 		else
 			throw new IllegalArgumentException("Voce não tem territorio com fronteira");
 		return false;
+	}
+	
+	public boolean realizarAtaque(String nomeTerritorioAtacante, String nomeTerritorioDefensor) throws Exception
+	{
+		if(partida.podemAtacar(nomeTerritorioAtacante, nomeTerritorioDefensor))
+		{
+			//pergunta pra view com quantos exercitos voce quer atacar
+			int qtdTropasAtaque = 0;
+			if(qtdTropasAtaque > 3)
+				throw new Exception("Voce nao pode atacar com mais de 3 tropas");
+			
+			if(partida.processaAtaque(nomeTerritorioAtacante, nomeTerritorioDefensor, qtdTropasAtaque))
+			{
+				// Atacante venceu
+				// remaneja tropas pra dentro do novo territorio
+				return true;
+			}
+			else
+				return false;
+		}
+		else
+			throw new Exception("Voce nao pode atacar esse territorio");
 	}
 }
