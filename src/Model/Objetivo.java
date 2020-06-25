ae3ea141966976cb2943f73de2b8bb813cdde9aa
Stefano <Stefano@DESKTOP-5L2O3HO>
2020-06-24 22:00:02 -0300

    refatoracao objetivos

--- src/Model/Objetivo.java
+++ src/Model/Objetivo.java
@@ -1,28 +1,6 @@
 package Model;
 
-import java.util.Random;
-
 public abstract class Objetivo 
-{
-	
-	static public Objetivo CreateNew()
-	{
-		Random rand = new Random();
-		switch(rand.nextInt(3))
-		{
-			case 0:
-				return new ObjetivoContinente();
-			case 1:
-				return new ObjetivoTerritorio();
-			case 2:
-				return new ObjetivoJogador();
-			default:
-				// Nunca deve acontecer, mas o compiler pede um default.
-				return null;
-					
-		}
-	}
-	
-	public abstract boolean verificarVitoria(Jogador jogador);
-	
+{	
+	public abstract boolean verificarVitoria(Jogador jogador);	
 }
