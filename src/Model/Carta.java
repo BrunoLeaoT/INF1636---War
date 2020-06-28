9b1497562adefbbedca7dc101a7fd47d402a9423
Stefano <Stefano@DESKTOP-5L2O3HO>
2020-06-28 12:40:36 -0300

    podeTrocarCarta movido de Carta para Jogador

--- src/Model/Carta.java
+++ src/Model/Carta.java
@@ -4,8 +4,8 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Random;
 
-class Carta {
-	private int numeroDeTrocas = 4;
+class Carta 
+{
 	private String nomeTerritorio;
 	private CartaForma forma;
 	
@@ -13,37 +13,12 @@ class Carta {
 		return nomeTerritorio;
 	}
 	
-	public String getCartaForma() {
-		return forma.name();
+	public CartaForma getCartaForma() {
+		return forma;
 	}
 	
 	public Carta(String nome,CartaForma forma) {
 		nomeTerritorio = nome;
 		this.forma = forma;
-	}
-	
-	public int podeTrocarCarta(ArrayList<Carta> cartas) {
-		int numQuadrado = 0, numCirculo = 0, numTriangulo = 0;
-		if(cartas.size() < 3)
-			throw new IllegalArgumentException("Quantidade de cartas deve ser maior ou igual a 3");
-		
-		for (int i = 0; i < cartas.size(); i++) {
-			if(cartas.get(i).forma.compareTo(CartaForma.Quadrado) == 0)
-				numQuadrado++;
-			
-			else if(cartas.get(i).forma.compareTo(CartaForma.Triangulo) == 0)
-				numTriangulo++;
-			
-			else if(cartas.get(i).forma.compareTo(CartaForma.Circulo) == 0)
-				numCirculo++;
-			else
-				throw new IllegalArgumentException("Esta forma não existe em cartas");
-	
-			if((numCirculo >= 3 || numQuadrado >= 3 || numTriangulo >= 3) || (numCirculo >=1 && numTriangulo >= 1 && numQuadrado >= 1)) {
-				numeroDeTrocas++;
-				return numeroDeTrocas;
-			}
-		}
-		return 0;
 	}
 }
