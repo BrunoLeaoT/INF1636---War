96b3418c6bf092fafd47380f2f9a240f25b7efa6
Stefano <Stefano@DESKTOP-5L2O3HO>
2020-07-05 05:06:47 -0300

    atualizado Tabuleiro view e controller

--- src/Controller/TabuleiroController.java
+++ src/Controller/TabuleiroController.java
@@ -1,6 +1,8 @@
 package Controller;
 
 import Model.Partida;
+import View.CartaView;
+import View.ObjetivoView;
 
 public class TabuleiroController 
 {
@@ -9,5 +11,18 @@ public class TabuleiroController
 	public TabuleiroController() 
 	{
 		partida = Partida.getInstancia();
+	}
+	
+	public void tryShowObjetivo()
+	{
+		String textoObjetivo = partida.getTextoObjetivoJogadorDaVez();
+		ObjetivoView ov = new ObjetivoView(textoObjetivo);
+		ov.setVisible(true);
+	}
+	
+	public void tryShowCartas()
+	{
+		CartaView cv = new CartaView(new CartaController());
+		cv.setVisible(true);
 	}
 }
--- src/View/TabuleiroView.java
+++ src/View/TabuleiroView.java
@@ -1,6 +1,11 @@
 package View;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 import Controller.TabuleiroController;
 
@@ -30,6 +35,10 @@ public class TabuleiroView extends JFrame
 		
 		// objects
 		addPainelFundo();
+		addBotaoPassarTurno();
+		addBotaoSalvarJogo();
+		addBotaoVerObjetivo();
+		addBotaoVerCartas();
 		
 	}
 	
@@ -40,4 +49,91 @@ public class TabuleiroView extends JFrame
 		painelFundo = new ImagePanel(caminhoImagemMar, caminhoImagemMapa);
 		this.add(painelFundo);
 	}
+	
+	private void addBotaoPassarTurno()
+	{
+		JButton botaoPassarTurno = new JButton("Acabar turno"); 
+		botaoPassarTurno.setBounds((LARG_DEFAULT/2) - 105, 625, 210, 50);
+		botaoPassarTurno.addActionListener(new ActionListener() 
+        {
+        	public void actionPerformed(ActionEvent e) 
+        	{
+        		try 
+        		{
+        			System.out.println("Passar vez");
+        		}
+        		catch(Exception ex)
+        		{
+        			JOptionPane.showMessageDialog(new JFrame(), ex.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE);
+        		}
+        	}
+        });
+        painelFundo.add(botaoPassarTurno);
+	}
+	
+	private void addBotaoSalvarJogo()
+	{
+		JButton botaoSalvarJogo = new JButton("Salvar jogo"); 
+		botaoSalvarJogo.setBounds(10, 10, 100, 25);
+		botaoSalvarJogo.addActionListener(new ActionListener() 
+        {
+        	public void actionPerformed(ActionEvent e) 
+        	{
+        		try 
+        		{
+        			System.out.println("Salvar jogo");
+        		}
+        		catch(Exception ex)
+        		{
+        			JOptionPane.showMessageDialog(new JFrame(), ex.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE);
+        		}
+        	}
+        });
+        painelFundo.add(botaoSalvarJogo);
+	}
+	
+	private void addBotaoVerObjetivo()
+	{
+		JButton botaoVerObjetivo = new JButton("Ver Objetivo"); 
+		botaoVerObjetivo.setBounds(890, 10, 110, 25);
+		botaoVerObjetivo.addActionListener(new ActionListener() 
+        {
+        	public void actionPerformed(ActionEvent e) 
+        	{
+        		try 
+        		{
+        			System.out.println("Ver objetivo");
+        			viewController.tryShowObjetivo();
+        		}
+        		catch(Exception ex)
+        		{
+        			JOptionPane.showMessageDialog(new JFrame(), ex.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE);
+        		}
+        	}
+        });
+        painelFundo.add(botaoVerObjetivo);
+	}
+	
+	private void addBotaoVerCartas()
+	{
+		JButton botaoVerCartas = new JButton("Ver Cartas"); 
+		botaoVerCartas.setBounds(890, 45, 110, 25);
+		botaoVerCartas.addActionListener(new ActionListener() 
+        {
+        	public void actionPerformed(ActionEvent e) 
+        	{
+        		try 
+        		{
+        			System.out.println("Ver cartas");
+        			viewController.tryShowCartas();
+        		}
+        		catch(Exception ex)
+        		{
+        			JOptionPane.showMessageDialog(new JFrame(), ex.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE);
+        		}
+        	}
+        });
+        painelFundo.add(botaoVerCartas);
+	}
+	
 }
