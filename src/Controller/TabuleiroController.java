dbbd83689b9a8fb082f386390c84c4f6bfd15e4a
Stefano <Stefano@DESKTOP-5L2O3HO>
2020-07-07 04:28:12 -0300

    pequenos avancos

--- src/Controller/TabuleiroController.java
+++ src/Controller/TabuleiroController.java
@@ -1,16 +1,27 @@
 package Controller;
 
+import java.awt.Point;
+import java.util.HashMap;
+
+import Model.Observador;
 import Model.Partida;
 import View.CartaView;
 import View.ObjetivoView;
+import View.TabuleiroView;
 
 public class TabuleiroController 
 {
+	private TabuleiroView tabView;
 	private Partida partida;
 	
 	public TabuleiroController() 
 	{
 		partida = Partida.getInstancia();
+	}
+	
+	public void addObservadorPartidaSelecaoLabel(Observador obs)
+	{
+		partida.addObservador(obs);
 	}
 	
 	public void tryShowObjetivo()
@@ -24,5 +35,16 @@ public class TabuleiroController
 	{
 		CartaView cv = new CartaView(new CartaController());
 		cv.setVisible(true);
+	}
+	
+	public void tryAcabarVez()
+	{
+		partida.passaTurno();
+	}
+	
+	public void trySelecionarTerritorio(int x, int y)
+	{
+		Point p = new Point(x,y);
+		partida.selecionaTerritorio(p);
 	}
 }
